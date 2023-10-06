6791f114b51ce443a3d9ee05f618bdfbfe433153
trandinhdan <146189514+DanTD-HE176813@users.noreply.github.com>
2023-10-06 14:00:07 +0700

    Create object departments
    object have did and dname

---
+++ Assignment_PRJ301/src/java/objects/Departments.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package objects;
+
+/**
+ *
+ * @author Laptop
+ */
+public class Departments {
+    private int did;
+    private String dname;
+
+    public int getDid() {
+        return did;
+    }
+
+    public void setDid(int did) {
+        this.did = did;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+    
+    
+}
