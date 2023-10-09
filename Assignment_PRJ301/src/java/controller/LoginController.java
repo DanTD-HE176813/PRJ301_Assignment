2e511e38f6f9445a34fe6bf3765cc3ed0008812f
trandinhdan <146189514+DanTD-HE176813@users.noreply.github.com>
2023-10-09 12:28:07 +0700

    Create file to login function
    Copy lecturers SonNT's code

---
+++ Assignment_PRJ301/src/java/controller/LoginController.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dal.UserDBContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import dal.User;
+
+/**
+ *
+ * @author Laptop
+ */
+@WebServlet(name = "LoginController", urlPatterns = {"/login"})
+public class LoginController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        request.getRequestDispatcher("view/login.jsp").forward(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        User param = new User();
+        param.setUsername(username);
+        param.setPassword(password);
+        UserDBContext db = new UserDBContext();
+        User loggedUser = db.get(param);
+        if(loggedUser != null)
+        {
+            response.getWriter().println("Hello " + loggedUser.getUsername());
+        }
+        else
+        {
+            response.getWriter().println("invalid username or password!");
+        }
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- Assignment_PRJ301/web/view/login.jsp
+++ Assignment_PRJ301/web/view/login.jsp
@@ -55,10 +55,10 @@
         <h2>Đăng Nhập</h2>
         <form action="login" method="post">
             <label for="username">Tên đăng nhập:</label>
-            <input type="text" id="username" name="username" required>
+            <input type="text" name="username">
 
             <label for="password">Mật khẩu:</label>
-            <input type="password" id="password" name="password" required>
+            <input type="password" name="password">
 
             <button type="submit">Đăng Nhập</button>
         </form>
